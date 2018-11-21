f74186bb4fa1ec89e1b301ef1deaf42243361c31
Mahesh Durgam <mdurgam@fcmat.org>
2018-11-21 13:12:37 -0800

    Simple .Net Core 2.1 Web Api

---
+++ SimpleDotnetCoreWebApi/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleDotnetCoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET api/users
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "User1", "User2" };
+        }
+
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "User" + id;
+        }      
+    }
+}
---
+++ SimpleDotnetCoreWebApi/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace SimpleDotnetCoreWebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new Info()
+                {
+                    Title = "Simple Web API",
+                    Description = "Simple .Net Core 2.1 Web API",
+                    Version = "V1",
+                    TermsOfService = "none"
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Simple Web API");
+            });
+
+            app.UseMvc();
+        }
+    }
+}
